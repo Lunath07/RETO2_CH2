f018dd0230fffb01b1a5bd2a4eca8c5ebdbed3dd
rafafuentes01 <75148995+rafafuentes01@users.noreply.github.com>
2023-11-29 22:13:31 -0500

    Update Camaras y Checkpoint
    Update Camaras y Checkpoint

--- RETO2_CH2FINAL/Assets/Scripts/CarController.cs
+++ RETO2_CH2FINAL/Assets/Scripts/CarController.cs
@@ -22,12 +22,15 @@ public class CarController : MonoBehaviour
 
     private float dragOnGround;
     public float gravityMod = 10f;
-    
+
     public Transform leftFrontWheel, rightFrontWheel;
     public float maxWheelTurn = 25f;
-    
-   
-   
+
+    public int nextCheckpoint;
+    public int currentLap;
+
+    public float lapTime, bestLapTime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,37 +38,42 @@ public class CarController : MonoBehaviour
 
         dragOnGround = theRB.drag;
 
+        UIManager.instance.lapCounterText.text = currentLap + "/" + RaceManager.instance.totalLaps;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-                speedInput = 0f;
-                if (Input.GetAxis("Vertical") > 0)
-                {
-                    speedInput = Input.GetAxis("Vertical") * forwardAccel;
-                }
-                else if (Input.GetAxis("Vertical") < 0)
-                {
-                    speedInput = Input.GetAxis("Vertical") * reverseAccel;
-                }
+        lapTime += Time.deltaTime;
+        var ts = System.TimeSpan.FromSeconds(lapTime);
+        UIManager.instance.currentLapTimeText.text = string.Format("{0:00}m{1:00}.{2:000}s", ts.Minutes, ts.Seconds, ts.Milliseconds);
 
-                turnInput = Input.GetAxis("Horizontal");
+        speedInput = 0f;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseAccel;
+        }
 
+        turnInput = Input.GetAxis("Horizontal");
+        /*
                  if(grounded && Input.GetAxis("Vertical") != 0)
                 {
                     transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Mathf.Sign(speedInput) * (theRB.velocity.magnitude / maxSpeed), 0f));
                 } 
           
-            
-            leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn) - 180, leftFrontWheel.localRotation.eulerAngles.z);
-            rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn), rightFrontWheel.localRotation.eulerAngles.z);
+            */
+        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn) - 180, leftFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn), rightFrontWheel.localRotation.eulerAngles.z);
 
-            transform.position = theRB.position;
-               
+        //transform.position = theRB.position;
+
     }
-    
+
 
     private void FixedUpdate()
     {
@@ -74,39 +82,40 @@ public class CarController : MonoBehaviour
         RaycastHit hit;
         Vector3 normalTarget = Vector3.zero;
 
-        if(Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround))
+        if (Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround))
         {
             grounded = true;
 
             normalTarget = hit.normal;
         }
 
-        if(Physics.Raycast(groundRayPoint2.position, -transform.up, out hit, groundRayLength, whatIsGround))
+        if (Physics.Raycast(groundRayPoint2.position, -transform.up, out hit, groundRayLength, whatIsGround))
         {
             grounded = true;
 
             normalTarget = (normalTarget + hit.normal) / 2f;
         }
 
-      
-        if(grounded)
+
+        if (grounded)
         {
             transform.rotation = Quaternion.FromToRotation(transform.up, normalTarget) * transform.rotation;
         }
-       
+
         if (grounded)
         {
             theRB.drag = dragOnGround;
 
             theRB.AddForce(transform.forward * speedInput * 1000f);
-        } else
+        }
+        else
         {
             theRB.drag = .1f;
 
             theRB.AddForce(-Vector3.up * gravityMod * 100f);
         }
 
-        if(theRB.velocity.magnitude > maxSpeed)
+        if (theRB.velocity.magnitude > maxSpeed)
         {
             theRB.velocity = theRB.velocity.normalized * maxSpeed;
         }
@@ -122,5 +131,36 @@ public class CarController : MonoBehaviour
 
     }
 
- 
-}
+    public void CheckpointHit(int cpNumber)
+    {
+        if (cpNumber == nextCheckpoint)
+        {
+            nextCheckpoint++;
+
+            if (nextCheckpoint == RaceManager.instance.allCheckpoints.Length)
+            {
+                nextCheckpoint = 0;
+                LapCompleted();
+            }
+        }
+
+
+    }
+    public void LapCompleted()
+    {
+        currentLap++;
+
+        if (lapTime < bestLapTime || bestLapTime == 0)
+        {
+            bestLapTime = lapTime;
+        }
+
+        lapTime = 0f;
+
+        var ts = System.TimeSpan.FromSeconds(bestLapTime);
+        UIManager.instance.bestLapTimeText.text = string.Format("{0:00}m{1:00}.{2:000}s", ts.Minutes, ts.Seconds, ts.Milliseconds);
+
+        UIManager.instance.lapCounterText.text = currentLap + "/" + RaceManager.instance.totalLaps;
+    }
+
+ }
